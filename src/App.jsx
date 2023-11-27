f11c876301484bae94ce908b4d4c5268304126b8
Kamil Raza <m.kamilrazabhatti@gmail.com>
2023-11-27 20:18:58 +0500

    about page almost done

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,8 @@ import About from "./Sections/About";
 const App = () => {
   return (
     <main>
-      <div className="main">
-        <div className="overlay">
-          <HeroSection />
-          <About />
-        </div>
-      </div>
+      <HeroSection />
+      <About />
     </main>
   );
 };
--- src/Sections/About.jsx
+++ src/Sections/About.jsx
@@ -1,71 +1,30 @@
-import React from 'react'
+import React from "react";
+import aboutImage from "/aboutImage.png";
+import { IoMailOutline } from "react-icons/io5";
 
 const About = () => {
   return (
-    <main>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-        <div>kamil</div>
-v
-        
-
-        <div></div>
+    <main className="wrapper flex items-center flex-col gap-5 md:justify-between md:flex-row">
+      <div className="flex flex-col justify-center gap-3">
+        <h4 className="text-mk-secondary mb-2">Hello! Welcome</h4>
+        <h1 className="text-[25px] sm:text-4xl lg:text-5xl font-bold">
+          I'm M.Kamil Raza
+        </h1>
+        <h1 className="text-[25px] sm:text-4xl lg:text-5xl font-bold">
+          Web Developer
+        </h1>
+        <div>
+          <button>
+            <IoMailOutline />
+            Hire Me
+          </button>
+        </div>
+      </div>
+      <div>
+        <img src={aboutImage} alt="Image" className="w-[400px] max-w-full" />
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default About
+export default About;
